3ec6bbf33ea40e282fc04d62407eeb8c2089a849
Venkatraj Bachu <62256896+Venkatraj-Bachu@users.noreply.github.com>
2023-10-17 22:33:35 -0400

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 20, 'normal')
 
@@ -10,8 +11,19 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.goto((0, 270))
 
     def update_screen(self):
         self.clear()
-        self.write(f"Score: {self.score}", align='center', font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align='center', font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode='w') as file:
+                file.write(str(self.high_score))
+
+        self.score = 0
+        self.update_screen()
